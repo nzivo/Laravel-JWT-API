38b0351c3a1b7356c4c0adfe9a1a0284b8889e4f
nzivo <johnnnzivo@gmail.com>
2020-07-31 13:07:11 +0300

    User details update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->bigInteger('phone')->unique();
+            $table->string('occupation');
+            $table->string('institution');
             $table->rememberToken();
             $table->timestamps();
         });
