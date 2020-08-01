4151f7b7acc1c3c358884cecc7f886da17d53314
nzivo <johnnnzivo@gmail.com>
2020-08-01 08:17:39 +0300

    Role Implementation

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Role;
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create(['name'=>'admin']);
+        Role::create(['name'=>'director']);
+        Role::create(['name'=>'librarian']);
+        Role::create(['name'=>'user']);
+    }
+}
