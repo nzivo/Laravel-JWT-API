7fcd8aff3dd184e8ff4e7f4824253d33f65355b5
nzivo <johnnnzivo@gmail.com>
2020-08-19 17:06:07 +0300

    Created Book table

---
+++ database/migrations/2020_08_19_132751_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('book_name');
+            $table->string('book_description');
+            $table->string('book_author');
+            $table->string('book_publisher');
+            $table->integer('book_pages');
+            $table->integer('book_year');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('book_type_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
