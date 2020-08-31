88e1865012ca445f3e7e5b6dc73411cb09be0ec9
nzivo <johnnnzivo@gmail.com>
2020-08-31 20:12:47 +0300

    Unique Fields

--- database/migrations/2020_08_19_125830_create_categories_table.php
+++ database/migrations/2020_08_19_125830_create_categories_table.php
@@ -15,7 +15,7 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('category_name');
+            $table->string('category_name')->unique();
             $table->string('category_description');
             $table->string('category_status');
             $table->timestamps();
--- database/migrations/2020_08_19_131446_create_book_types_table.php
+++ database/migrations/2020_08_19_131446_create_book_types_table.php
@@ -15,7 +15,7 @@ class CreateBookTypesTable extends Migration
     {
         Schema::create('book_types', function (Blueprint $table) {
             $table->id();
-            $table->string('type_name');
+            $table->string('type_name')->unique();
             $table->string('type_description');
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->timestamps();
