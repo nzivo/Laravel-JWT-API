1798193a2fd4e25811e08d6ce41cd771c780090c
nzivo <johnnnzivo@gmail.com>
2020-09-03 17:55:28 +0300

    Database Factories Added

---
+++ database/factories/BookTypeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\BookType;
+use Faker\Generator as Faker;
+
+$factory->define(BookType::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
