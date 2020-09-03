dbaa77fe7132cc29355406777e9476220befb37a
nzivo <johnnnzivo@gmail.com>
2020-09-03 17:55:55 +0300

    Category and book type Added

--- routes/api.php
+++ routes/api.php
@@ -24,4 +24,10 @@ Route::get('logout', 'AuthController@logout');
 Route::post('save_user_info', 'AuthController@saveUserInfo')->middleware('jwtAuth');
 
 Route::apiResource('user', 'API\UserController');
+
 Route::apiResource('category', 'API\CategoryController');
+Route::group(['prefix'=>'category'],function(){
+    Route::apiResource('/{category}/bookTypes','API\BookTypeController');
+});
+
+Route::apiResource('bookType', 'API\BookTypeController');
