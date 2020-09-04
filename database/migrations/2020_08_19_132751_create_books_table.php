8bc4234a5c278659076887141f38e150b058f39b
nzivo <johnnnzivo@gmail.com>
2020-09-04 12:07:25 +0300

    Added two Unique fields

--- database/migrations/2020_08_19_132751_create_books_table.php
+++ database/migrations/2020_08_19_132751_create_books_table.php
@@ -15,13 +15,15 @@ class CreateBooksTable extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->string('book_photo');
+            $table->string('book_photo')->default('default.jpg');
             $table->string('book_name');
             $table->string('book_description');
             $table->string('book_author');
             $table->string('book_publisher');
             $table->integer('book_pages');
             $table->integer('book_year');
+            $table->string('catalogue_no');
+            $table->string('accession_no')->unique();
             $table->string('book_status')->default('Available');
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->foreignId('book_type_id')->constrained()->onDelete('cascade');
