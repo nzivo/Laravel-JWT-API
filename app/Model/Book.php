6f0ee2f40ce12c5f25f1113a18c5138adb787443
nzivo <johnnnzivo@gmail.com>
2020-09-04 12:08:52 +0300

    Added relations and fields

--- app/Model/Book.php
+++ app/Model/Book.php
@@ -2,9 +2,27 @@
 
 namespace App\Model;
 
+use App\Model\BookType;
+use App\Model\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    protected $fillable = [
+        'book_photo','book_name','book_description','book_author',
+        'book_publisher','book_pages','book_year','book_status',
+        'category_id','book_type_id',
+    ];
+    public function category()
+    {
+        # code...
+        return $this->belongsTo(Category::class);
+    }
+
+    public function book_type()
+    {
+        # code...
+        return $this->belongsTo(BookType::class);
+    }
+
 }
